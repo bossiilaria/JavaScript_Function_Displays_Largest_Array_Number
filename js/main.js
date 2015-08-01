5b67fe75223ef83abc38b1526d93b313cdfecb2b
bossiilaria <odietamo.bossi200@gmail.com>
2015-08-01 18:25:06 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+	/*Write a function that displays the largest number in an array*/
+	
+	var nums = [3, 56, 7];
+
+	
+	/*
+	var num2 = window.prompt("Enter the second number");
+	
+	var num3 = num1 * num2;
+
+	*/
